007278f28da45d06b82323ad99b6798567e85ae9
Rudranil Das <rudranild04@gmail.com>
2025-06-25 08:47:33 +0530

    Refactor comments for clarity and organization in Boolean class

--- Boolean.java
+++ Boolean.java
@@ -1,12 +1,24 @@
+// Java Program to Demonstrate Boolean Primitive DataType
+
+// Class
 public class Boolean {
+    // Main driver method
     public static void main(String[] args){
+        // Boolean data type is a data type that has one of
+        // two possible values (usually denoted true and
+        // false).
+        // Setting boolean to false and true initially
         boolean a = false;
         boolean b = true;
 
+        // If condition holds
         if(a == false){
+            // Print statement
             System.out.println("Hello Rudra!");        
         }
+        // If condition holds
         if(b == true){
+            // Print statement
             System.out.println("Hi Rudra");
         }
     }
