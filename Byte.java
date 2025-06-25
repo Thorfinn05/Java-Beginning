52fff938528bfce3cdc18de4187ace4c03bfc0fe
Rudranil Das <rudranild04@gmail.com>
2025-06-25 08:51:36 +0530

    Add Byte class with main method demonstrating byte increment

---
+++ Byte.java
@@ -0,0 +1,19 @@
+public class Byte {
+    public static void main(String[] args){
+        byte a = 126;
+
+        // byte is 8 bit value
+        System.out.println(a);
+
+        // byte is still 8 bit value
+        a++;
+        System.out.println(a);
+
+        
+        a++;
+        System.out.println(a);
+
+        a++;
+        System.out.println(a);
+    }
+}
