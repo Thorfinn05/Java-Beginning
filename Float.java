88eccfb1269d2468f1659a2b8437f9e320bdcbd6
Rudranil Das <rudranild04@gmail.com>
2025-06-25 09:00:04 +0530

    Improve comments in Float class to clarify float value usage

--- Float.java
+++ Float.java
@@ -2,5 +2,11 @@ public class Float {
     public static void main(String[] args){
         float a = 9.8f;
         System.out.println(a);
+
+        // float value1 = 9.87;
+        // Print statement
+        // System.out.println(value1);
+        //Output will face an error as 9.8 is suggsting a conversion of 'double' to 'float'
+        //That is why 9.8'f' is being written
     }
 }
