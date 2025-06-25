33d72d19a4eac9e941da2281f9ddf12c8ee8fc40
Rudranil Das <rudranild04@gmail.com>
2025-06-25 08:45:43 +0530

    Add Boolean class with main method and basic boolean logic

---
+++ Boolean.java
@@ -0,0 +1,13 @@
+public class Boolean {
+    public static void main(String[] args){
+        boolean a = false;
+        boolean b = true;
+
+        if(a == false){
+            System.out.println("Hello Rudra!");        
+        }
+        if(b == true){
+            System.out.println("Hi Rudra");
+        }
+    }
+}
