1aa4282f22f60f98d7464631470647d43fe9cd06
Rudranil Das <rudranild04@gmail.com>
2025-08-07 08:44:55 +0530

    Add output comment to clarify comparison of Car instances

--- Main.java
+++ Main.java
@@ -9,6 +9,6 @@ public class Main {
     public static void main(String args[]){
         Car car1 =  new Car ("BMW");
         Car car2 = new Car ("BMW");
-        System.out.println(car1 == car2);
+        System.out.println(car1 == car2);  // Output: false
     }
 }
