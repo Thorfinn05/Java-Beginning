f29167179aa1f84c28b338f4c5eb584af47d3391
Rudranil Das <rudranild04@gmail.com>
2025-08-07 08:44:22 +0530

    Add Car class and implement main method to compare car instances

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Car {
+    String brand;
+    Car(String brand){
+        this.brand = brand;
+    }
+}
+
+public class Main {
+    public static void main(String args[]){
+        Car car1 =  new Car ("BMW");
+        Car car2 = new Car ("BMW");
+        System.out.println(car1 == car2);
+    }
+}
