b0bbca12c79415a8c83f66cb0259072e278a5afb
Thorfinn05 <rudranild04@gmail.com>
2025-09-06 22:04:35 +0530

    Refactor Honda class to extend Bike and override run method for smoother output

--- Honda.java
+++ Honda.java
@@ -1,7 +1,11 @@
-public class Honda {
-    final int speedlimit = 90;
+class Bike{
+    final void run(){
+        System.out.println("running");
+    }
+}
+public class Honda extends Bike {
     void run(){
-        speedlimit = 400;
+        System.out.println("running smoothly");
     }
     public static void main(String[] args) {
         Honda ob = new Honda();
