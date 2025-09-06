c14c77e936b54159d9a15bd9e87cdbabff9bc398
Thorfinn05 <rudranild04@gmail.com>
2025-09-06 22:05:05 +0530

    Remove overridden run method in Honda class to restore original behavior from Bike class

--- Honda.java
+++ Honda.java
@@ -4,9 +4,6 @@ class Bike{
     }
 }
 public class Honda extends Bike {
-    void run(){
-        System.out.println("running smoothly");
-    }
     public static void main(String[] args) {
         Honda ob = new Honda();
         ob.run();
