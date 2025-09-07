fef79b5aa73a8019229ba4f9b9cb1f85467a37d9
Thorfinn05 <rudranild04@gmail.com>
2025-09-07 16:36:26 +0530

    Add instantiation of third Counter object in main method

--- Counter.java
+++ Counter.java
@@ -7,5 +7,6 @@ public class Counter {
     public static void main(String[] args) {
         Counter c1 = new Counter();
         Counter c2 = new Counter();
+        Counter c3 = new Counter();
     }
 }
