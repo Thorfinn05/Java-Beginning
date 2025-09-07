ba07d8508037a81ec9f8db9298e0eb326723cfab
Thorfinn05 <rudranild04@gmail.com>
2025-09-07 20:05:03 +0530

    Add comments to clarify method declaration and usage in Interface_1.java

--- Interface_1.java
+++ Interface_1.java
@@ -1,5 +1,5 @@
 interface B{
-    int methodB(int i);
+    int methodB(int i);  //Method is declared having just the defn
 }
 
 class A implements B{
@@ -11,6 +11,6 @@ class A implements B{
 public class Interface_1 {
     public static void main(String[] args) {
         B ob = new A();
-        System.out.println(ob.methodB(2));
+        System.out.println(ob.methodB(2));  //Reference of type B holds object of type A
     }
 }
