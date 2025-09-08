334c52e5f0f2df511e32edecab7479cbc2adab6c
Thorfinn05 <rudranild04@gmail.com>
2025-09-08 23:28:05 +0530

    Implement stack operations: add pop and display methods, and enhance main method for user interaction

--- DSA/stack.java
+++ DSA/stack.java
@@ -17,9 +17,56 @@ class Operation{
         return head;
     }
 
-    Node pop(Node )
+    Node pop(Node head){
+        if(head == null){
+            System.out.println("Empty Stack!");
+        }
+        System.out.println("Popped element: " + head.data);
+        head = head.next;
+        return head;
+    }
+
+    void display(Node head){
+        if(head == null){
+            System.out.println("Empty Stack!");
+        }
+        Node ptr = head;
+        while(ptr != null){
+            System.out.println("| "+ptr.data+" |");
+            ptr = ptr.next;
+        }
+        System.out.println("-----");
+    }
 }
 
 public class stack {
-    
+    public static void main(String[] args) {
+        Node head = null;
+        Operation ob = new Operation();
+        System.out.println("---Stack Operations---");
+        System.out.println("1. Push");
+        System.out.println("2. Pop");
+        System.out.println("3. Display");
+        System.out.println("4. Exit");
+        int ch;
+        Scanner sc = new Scanner(System.in);
+        do { 
+            System.out.println("Enter choice: ");
+            ch = sc.nextInt();
+            switch(ch){
+                case 1:
+                    head = ob.push(head);
+                    break;
+                case 2:
+                    head = ob.pop(head);
+                    break;
+                case 3:
+                    ob.display(head);
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    return;
+            }
+        } while (true);
+    }
 }
